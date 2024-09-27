997254fe24a6ffee8375f45f54acbeed5994bb04
fajrCode <fajri42vandi@gmail.com>
2024-09-27 22:32:19 +0700

    refactor: Bank account | move all validation and handler to bank system

--- bank_account.js
+++ bank_account.js
@@ -1,65 +1,32 @@
-const { InvalidInput, InvalidAmount } = require('./custom_error');
-
 class BankAccount {
-    constructor(nama) {
+    constructor(nama, nomorRekening) {
         this.nama = nama;
-        this.accountnumber = Math.floor(Math.random() * 1000000000);
+        this.nomorRekening = nomorRekening;
         this.saldo = 0;
     }
 
-    // protected method to deposit money
-    _deposit(amount) {
-        if (amount === null) return;
-
-        if (isNaN(amount) || !/^\d+$/.test(amount))
-            throw new InvalidInput('Maaf, inputan tidak valid. Hanya angka yang diperbolehkan.');
-        if (amount <= 0)
-            throw new InvalidAmount('Maaf, jumlah saldo tidak boleh kurang atau sama dengan 0.');
-
+    deposit(amount) {
         this.saldo += parseFloat(amount);
-
-        return `Selamat saldo berhasil ditambahkan. \nSaldo anda sekarang: ${this.#formatRupiah(this.saldo)}`;
+        return this.formatRupiah(this.saldo);
     }
 
-    // protected method to withdraw money
-    _withdraw(amount) {
-        if (amount === null) return;
-
-        if (isNaN(amount) || !/^\d+$/.test(amount))
-            throw new InvalidInput('Maaf, inputan tidak valid. Hanya angka yang diperbolehkan.');
-
-        if (amount <= 0)
-            throw new InvalidAmount('Maaf, jumlah saldo tidak boleh kurang atau sama dengan 0.');
-
-        if (amount > this.saldo)
-            throw new InvalidAmount(`Maaf, saldo anda tidak cukup.\nSaldo anda sekarang: ${this.#formatRupiah(this.saldo)}`);
-
+    withdraw(amount) {
         this.saldo -= parseFloat(amount);
-
-        return `Selamat saldo berhasil dikurangi. \nSaldo anda sekarang: ${this.#formatRupiah(this.saldo)}`
+        return this.formatRupiah(this.saldo);
     }
 
-    // protected method get saldo
-    _getSaldo() {
-        return `Saldo anda sekarang: ${this.#formatRupiah(this.saldo)}`;
+    getSaldo() {
+        return this.formatRupiah(this.saldo);
     }
 
-    // private method to format number to rupiah
-    #formatRupiah(number) {
+    formatRupiah(number) {
         let rupiah = "";
         const numberrev = number.toString().split("").reverse().join("");
         for (let i = 0; i < numberrev.length; i++) {
             if (i % 3 === 0) rupiah += numberrev.substr(i, 3) + ".";
         }
-        return (
-            "Rp. " +
-            rupiah
-                .split("", rupiah.length - 1)
-                .reverse()
-                .join("")
-        );
+        return "Rp. " + rupiah.split("", rupiah.length - 1).reverse().join("");
     }
-
 }
 
 module.exports = BankAccount;
