07c90c4784e98c665d276d0a782b3e0b06f6a4da
fajrCode <fajri42vandi@gmail.com>
2024-09-27 21:42:28 +0700

    feat: bank system | test input in terminal

--- bank_system.js
+++ bank_system.js
@@ -1,7 +1,28 @@
 const BankAccount = require('./bank_account');
+const readline = require('readline');
 
-const bankAccount = new BankAccount('John Doe');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(bankAccount.deposit(1000000));
-console.log(bankAccount.deposit('1000000'));
-console.log(bankAccount.deposit(0));
+const input = (text) => {
+    return new Promise((resolve) => rl.question(text, resolve));
+};
+
+async function main() {
+    const nama = await input('Masukkan nama anda: ');
+    const bankAccount = new BankAccount(nama);
+
+    console.log(nama, bankAccount.accountnumber);
+    console.log(bankAccount.deposit(1000000));
+    console.log(bankAccount.deposit('1000000'));
+    console.log(bankAccount.deposit(0));
+}
+
+main();
+// const bankAccount = new BankAccount('John Doe');
+
+// console.log(bankAccount.deposit(1000000));
+// console.log(bankAccount.deposit('1000000'));
+// console.log(bankAccount.deposit(0));
