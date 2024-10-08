a27e13d805b013d6be341edcef2d7fc963d00772
fajrCode <fajri42vandi@gmail.com>
2024-10-08 21:03:59 +0700

    refactor: adjust table account | change lenght balance

--- db/migrations/2_create_table_account.sql
+++ db/migrations/2_create_table_account.sql
@@ -2,10 +2,10 @@ CREATE TYPE account_type AS ENUM ('silver', 'gold', 'platinum');
 
 CREATE TABLE IF NOT EXISTS accounts (
     id SERIAL PRIMARY KEY,
-    account_number VARCHAR(50) NOT NULL,
+    account_number VARCHAR(20) NOT NULL,
     customer_id INT REFERENCES customers(id),
     type account_type NOT NULL,
-    balance DECIMAL(10, 2) NOT NULL,
+    balance DECIMAL(15, 2) NOT NULL,
     status BOOLEAN NOT NULL DEFAULT TRUE
 );
 
