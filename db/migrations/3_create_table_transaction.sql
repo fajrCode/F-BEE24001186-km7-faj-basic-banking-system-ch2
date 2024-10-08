b32d98397acb2d84057d29c40baba40aceb09281
fajrCode <fajri42vandi@gmail.com>
2024-10-09 00:18:10 +0700

    perf: add indexing transactions account id and type

--- db/migrations/3_create_table_transaction.sql
+++ db/migrations/3_create_table_transaction.sql
@@ -12,4 +12,7 @@ ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE transactions
 ADD COLUMN updated_at TIMESTAMP;
 
+CREATE INDEX ON transactions (account_id);
+CREATE INDEX ON transactions (type);
+
 DROP TABLE IF EXISTS transactions;
