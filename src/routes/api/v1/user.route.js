fe371d6e016e24d0c9fe3016b3e70e9d24e45b20
fajrCode <fajri42vandi@gmail.com>
2024-10-18 17:55:35 +0700

    refactor: adjust routes folder

---
+++ src/routes/api/v1/user.route.js
@@ -0,0 +1,14 @@
+import UserCtrl from '../../../controllers/user.controller.js';
+
+export default class UserRoute {
+    constructor(router) {
+        this.router = router;
+        this.controller = new UserCtrl();
+        this.basepath = '/users';
+        this.initializeRoutes();
+    }
+
+    initializeRoutes() {
+        this.router.get(this.basepath + '/', this.controller.getAll);
+    }
+}
