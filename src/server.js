3b86b14ac244b2e8180d7674ec57c7778a1bcbf4
fajrCode <fajri42vandi@gmail.com>
2024-10-18 14:40:38 +0700

    package: install express-list-endpoints

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 import "dotenv/config";
 import http from "http";
-import { app } from "./app.js";
+import listEndpoints from 'express-list-endpoints';
+import { app } from "./configs/app.js";
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 try {
+    if (process.env.NODE_ENV == "development") {
+        console.log("================== API - LIST =======================");
+        listEndpoints(app).forEach((route) => {
+            route.methods.forEach((method) => {
+                console.log(`Route => ${method} ${route.path}`);
+            });
+        });
+        console.log("================== API - LIST =======================\n");
+    };
+
     server.listen(port, () => {
-        console.log(`🚀 Server is running on port ${port}`);
+        console.log(`🚀 Server is running on port ${port} '${process.env.NODE_ENV}'`);
     });
 } catch (error) {
     console.error(`Error: ${error.message}`);
