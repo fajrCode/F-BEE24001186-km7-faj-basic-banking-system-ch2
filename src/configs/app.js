da6019a5df6422b9c8b0a0c72c38f2acf0fa59b0
fajrCode <fajri42vandi@gmail.com>
2024-10-24 10:29:52 +0700

    chore: init config swagger

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import logger from './logger.js';
 import ErrorHandler from '../middlewares/errorHandler.js';
-import apiV1 from '../routes/api/v1/index.js';
+import apiV1 from '../routes/api/v1/index.route.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../routes/api/v1/api-doc.json' assert { type: "json" };
 
 export const app = express();
 
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/public', express.static('public'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.set('view engine', 'ejs');
 
 // API Router
