059ccecc6369d57b7ec2b6c6327964f6434bd00e
fajrCode <fajri42vandi@gmail.com>
2024-10-25 09:57:15 +0700

    refactor: move config swagger ui from app.js to index route api v1

--- src/configs/app.js
+++ src/configs/app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import logger from './logger.js';
 import ErrorHandler from '../middlewares/errorHandler.js';
 import apiV1 from '../routes/api/v1/index.route.js';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../routes/api/v1/api-doc.json' assert { type: "json" };
 
 export const app = express();
 
@@ -12,7 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/public', express.static('public'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.set('view engine', 'ejs');
 
 // API Router
--- src/routes/api/v1/index.route.js
+++ src/routes/api/v1/index.route.js
@@ -3,12 +3,15 @@ import { IndexCtrl } from '../../../controllers/index.controller.js';
 import UserRoute from './user.route.js';
 import AccountRoute from './account.route.js';
 import TransactionRoute from './transaction.route.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../routes/api/v1/api-doc.json' assert { type: "json" };
 
 export default (app) => {
     const router = Router();
     const index = new IndexCtrl();
-
+    
     app.use('/api/v1', router);
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     
     router.get('/', index.root);
 
