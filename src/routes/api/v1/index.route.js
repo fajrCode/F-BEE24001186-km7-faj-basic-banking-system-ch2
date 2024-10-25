9eaf2e0001c778192acf53a82303930f54307c29
fajrCode <fajri42vandi@gmail.com>
2024-10-25 16:14:24 +0700

    fix: call docs api v1

--- src/routes/api/v1/index.route.js
+++ src/routes/api/v1/index.route.js
@@ -4,14 +4,14 @@ import UserRoute from './user.route.js';
 import AccountRoute from './account.route.js';
 import TransactionRoute from './transaction.route.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../routes/api/v1/api-doc.json' assert { type: "json" };
+import swaggerDocument from '../../../docs/api-v1.json' assert { type: "json" };
 
 export default (app) => {
     const router = Router();
     const index = new IndexCtrl();
     
     app.use('/api/v1', router);
-    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     
     router.get('/', index.root);
 
