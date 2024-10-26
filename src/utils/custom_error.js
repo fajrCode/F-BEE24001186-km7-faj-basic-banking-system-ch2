dcd4c040015c8d20a286cc5d24031d0116cb66b8
fajrCode <fajri42vandi@gmail.com>
2024-10-26 09:35:15 +0700

    feat: custom error | add new error 400

--- src/utils/custom_error.js
+++ src/utils/custom_error.js
@@ -4,3 +4,10 @@ export class ErrorDbInput extends Error {
         this.name = 'InvalidInput';
     }
 }
+
+export class Error400 extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'BadRequest';
+    }
+}
