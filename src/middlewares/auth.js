1a5f97529e2b417d3091647e60bb8e0d73316ef8
fajrCode <fajri42vandi@gmail.com>
2024-10-26 11:38:48 +0700

    feat: auth | create middleware auth

---
+++ src/middlewares/auth.js
@@ -0,0 +1,43 @@
+import ResponseHandler from "../utils/response.js";
+import jwt from "jsonwebtoken";
+
+export class Auth {
+    constructor() {
+        this.response = new ResponseHandler();
+    }
+
+    authenticate = (req, res, next)=> {
+        const { authorization } = req.headers;
+        const token = authorization.split(" ")[1] || authorization;
+
+        if (!token) {
+            return this.response.res401(res);
+        }
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const payload = {
+                id: decoded.id,
+                name: decoded.name,
+            };
+            req.user = payload;
+            next();
+        } catch (error) {
+            return this.response.res401(res);
+        }
+    }
+
+    checkLoginAuth = (req, res, next) => {
+        try {
+            const { authorization } = req.headers;
+            if(authorization){
+                return this.response.res401(res);
+            }
+            next();
+        } catch (error) {
+            console.log(error.message);
+            return this.response.res500(res);
+        }
+    }
+
+}
