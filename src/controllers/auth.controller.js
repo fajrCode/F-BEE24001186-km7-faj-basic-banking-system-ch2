2c0ad90e74c9f10a854ffd91fe951b2dad60300d
fajrCode <fajri42vandi@gmail.com>
2024-10-26 10:32:32 +0700

    feat: auth | complete

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,47 +1,53 @@
 import AuthService from "../services/auth.service.js";
-import loginValidator from "../validations/auth.validation.js";
+import { loginValidator, registerValidator } from "../validations/auth.validation.js";
+import ResponseHandler from "../utils/response.js";
+import { Error400 } from "../utils/custom_error.js";
 
 export default class AuthController {
     constructor() {
         this.test = "test";
         this.service = new AuthService();
+        this.response = new ResponseHandler();
         // this.login = this.login.bind(this); // Bind the login method if use function declaration (async login (req, res) {})
     }
 
     login = async (req, res) => {
         try {
-            console.log(this.test);
-            const data = await loginValidator.validateAsync(req.body);
-            const result = await this.service.login(data);
-            return res.status(200).json({
-                status: "success",
-                message: "Login success",
-                data: result,
-            });
+            const { error, value } = loginValidator.validate(req.body);
+
+            if (error) {
+                throw new Error400(`${error.details[0].message}`);
+            }
+
+            const result = await this.service.login(value);
+            return this.response.res200("Login success", result, res);
         } catch (error) {
-            const msg = error.message || "Internal Server Error";
-            return res.status(500).json({
-                status: "error",
-                message: msg,
-            });
+            if(error instanceof Error400) {
+                return this.response.res400(error.message, res);
+            } else {
+                console.log(error.message);
+                return this.response.res500(res);
+            }
         };
     };
 
     register = async (req, res) => {
         try {
-            const data = req.body;
-            const result = await this.service.register(data);
-            return res.status(200).json({
-                status: "success",
-                message: "Register success",
-                data: result,
-            });
+            const { error, value } = registerValidator.validate(req.body);
+
+            if (error) {
+                throw new Error400(`${error.details[0].message}`);
+            }
+
+            const result = await this.service.register(value);
+            return this.response.res200("Register success", result, res);
         } catch (error) {
-            const msg = error.message || "Internal Server Error";
-            return res.status(500).json({
-                status: "error",
-                message: msg,
-            });
+            if(error instanceof Error400) {
+                return this.response.res400(error.message, res);
+            } else {
+                console.log(error);
+                return this.response.res500(res);
+            }
         };
     };
 }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"; 
+import jwt from "jsonwebtoken";
 import prisma from "../configs/database.js";
 import UserService from "./user.service.js";
+import { Error400 } from "../utils/custom_error.js";
 
 export default class AuthService {
     constructor() {
@@ -10,43 +11,53 @@ export default class AuthService {
     }
 
     async login(data) {
-        const user = await this._model.findUnique({
-            where: {
-                email: data.email,
-            },
-        });
+        try {
+            const user = await this._model.findUnique({
+                where: {
+                    email: data.email,
+                },
+            });
 
-        if (!user) {
-            throw new Error("Email or password is wrong");
+            console.log(user);
+    
+            if (!user) {
+                throw new Error400("Email is wrong");
+            }
+    
+            const isPasswordMatch = await bcrypt.compare(data.password, user.password);
+    
+            if (!isPasswordMatch) {
+                throw new Error400("Password is wrong");
+            }
+    
+            const secretKey = process.env.JWT_SECRET || "secret";
+            const token = jwt.sign({ id: user.id, name: user.name }, secretKey, {
+                expiresIn: "1h",
+            });
+    
+            return { token };
+        } catch (err) {
+            if(err instanceof Error400) {
+                throw new Error400(err.message);
+            } else {
+                console.log(err.message);
+                throw new Error("Internal Server Error");
+            }
         }
-
-        const isPasswordMatch = await bcrypt.compare(data.password, user.password);
-
-        if (!isPasswordMatch) {
-            throw new Error("Email or password is wrong");
-        }
-
-        const secretKey = process.env.JWT_SECRET || "secret";
-        console.log(secretKey);
-        const token = jwt.sign({ id: user.id }, secretKey, {
-            expiresIn: "1h",
-        });
-
-        const result = {
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-            },
-            token,
-        }
-
-        return result;
     }
 
     async register(data) {
-        const userService = new UserService();
-        const user = await userService.create(data);
-        return user;
+        try {
+            const userService = new UserService();
+            const user = await userService.create(data);
+            return user;
+        } catch (err) {
+            if(err instanceof Error400) {
+                throw new Error400(err.message);
+            } else {
+                console.log(err.message);
+                throw new Error("Internal Server Error");
+            }
+        }
     }
 }
