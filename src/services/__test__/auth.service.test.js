9d1025ff03c7ea45abc17cc8d3fc2ec7ec4c817a
fajrCode <fajri42vandi@gmail.com>
2024-10-29 13:31:43 +0700

    test: auth service | coverage 100%

---
+++ src/services/__test__/auth.service.test.js
@@ -0,0 +1,94 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import AuthService from '../auth.service';
+import prisma from '../../configs/database.js';
+import { Error400 } from '../../utils/custom_error.js';
+
+jest.mock('bcrypt');
+jest.mock('jsonwebtoken');
+jest.mock('../../configs/database.js', () => ({
+    user: {
+        findUnique: jest.fn(),
+        create: jest.fn(),
+    },
+}));
+
+describe('Testing Auth Service', () => {
+    let authService;
+    let mockData;
+    let mockUser;
+
+    beforeEach(() => {
+        authService = new AuthService();
+        jest.clearAllMocks();
+        mockData = {
+            email: 'fulan',
+            password: 'password',
+        };
+        mockUser = {
+            id: 1,
+            name: 'fulan',
+            email: 'fulan',
+            password: 'password',
+        };
+    });
+
+    describe('Login', () => {
+        it('should return token when login success', async () => {
+            prisma.user.findUnique.mockResolvedValue(mockUser);
+            bcrypt.compare.mockResolvedValue(true);
+            jwt.sign.mockReturnValue('token');
+
+            const result = await authService.login(mockData);
+
+            expect(result).toEqual({ token: 'token' });
+        });
+
+        it('should throw error when email is wrong', async () => {
+            prisma.user.findUnique.mockResolvedValue(null);
+
+            await expect(authService.login(mockData)).rejects.toThrow(Error400);
+        });
+
+        it('should throw error when password is wrong', async () => {
+            prisma.user.findUnique.mockResolvedValue(mockUser);
+            bcrypt.compare.mockResolvedValue(false);
+
+            await expect(authService.login(mockData)).rejects.toThrow(Error400);
+        });
+
+        it('should throw error when login failed', async () => {
+            prisma.user.findUnique.mockRejectedValue(new Error('Internal Server Error'));
+
+            await expect(authService.login(mockData)).rejects.toThrow(Error);
+        });
+    });
+
+    describe('Register', () => {
+        it('should return user when register success', async () => {
+            prisma.user.create.mockResolvedValue(mockUser);
+
+            const result = await authService.register(mockData);
+
+            expect(result).toEqual(mockUser);
+        });
+
+        it('should throw error when register failed', async () => {
+            prisma.user.create.mockRejectedValue({
+                code: 'P2002',
+                message: 'Unique constraint violation'
+            });
+
+            await expect(authService.register(mockData)).rejects.toThrow(Error400);
+        });
+
+        it('should throw error when register failed', async () => {
+            prisma.user.create.mockRejectedValue(new Error('Internal Server Error'));
+
+            await expect(authService.register(mockData)).rejects.toThrow(Error);
+        });
+
+    });
+
+
+});
