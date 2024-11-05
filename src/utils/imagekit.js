e843bba4692cf53de0498e344e2388ccbfb84e53
fajrCode <fajri42vandi@gmail.com>
2024-11-05 11:13:02 +0700

    refactor: utils imagekit | remove condition test

--- src/utils/imagekit.js
+++ src/utils/imagekit.js
@@ -1,10 +1,7 @@
 import ImageKit from "imagekit";
 
-export const imagekit =
-    process.env.NODE_ENV === 'test'
-        ? { upload: jest.fn() }
-        : new ImageKit({
-            publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-            privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-            urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-        });
+export const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
