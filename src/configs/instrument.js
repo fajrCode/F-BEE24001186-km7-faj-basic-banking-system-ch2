0dededf632c275913d3f1043c2e2d3a92925d334
fajrCode <fajri42vandi@gmail.com>
2024-11-16 21:14:10 +0700

    refactor: instrument sentry | sentry dsn

--- src/configs/instrument.js
+++ src/configs/instrument.js
@@ -2,7 +2,7 @@ import { init } from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 init({
-  dsn: "https://255f0f3bbc6db54d5483aaccd28e247f@o4508289914372096.ingest.us.sentry.io/4508290338390016",
+  dsn: process.env.SENTRY_DSN,
   integrations: [
     nodeProfilingIntegration(),
   ],
